7a872855eeab4bf11930a469ac821ee491174ea8
Thiago Yasunaka <thiagoyasunaka@hotmail.com>
2021-08-15 10:41:06 -0300

    Graphs

---
+++ graphs/graph.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+# time without pthread
+xWithoutPThread = [512, 1024, 2056, 4096]
+yWithoutPThread = [1.42, 6.83, 42.62, 460.61]
+
+# time with pthread
+xPThread = [512, 1024, 2056, 4096]
+yPThread = [0.19009, 1.1, 11.32, 184.91]
+
+plt.plot(xWithoutPThread, yWithoutPThread,
+         label="Without 4 Threads", marker='o', linestyle='dashed')
+plt.plot(xPThread, yPThread, label="Using Threads",
+         marker='o', linestyle='dashed')
+
+plt.xlabel('Matrix Dimensions')
+plt.ylabel('Time - secs')
+
+plt.title('Speedup for Matrix Multiplication')
+
+plt.legend()
+
+plt.show()
