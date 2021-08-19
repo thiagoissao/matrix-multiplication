4efaec56b045fecea6c999ca6504036ba9021657
Thiago Yasunaka <thiagoyasunaka@hotmail.com>
2021-08-19 06:40:46 -0300

    novos testes

--- graphs/speedup-graphy.py
+++ graphs/speedup-graphy.py
@@ -1,50 +1,46 @@
 import matplotlib.pyplot as plt
 
-x = [2, 4, 8, 16]
-ySequencialTime = [1.42, 6.83, 42.62, 460.61]
-
 # tempo de execução para tamanhos 512, 1024, 2048, 4096
+sequencial = [0.31258, 2.76, 39.68, 460.61]
+x1Thread = [0.36267, 3.36, 43.47, 515.87]
 x2Threads = [0.201, 1.67, 19.34, 331.41]
-x4threads = [0.190, 1.1, 11.32, 184.91]
-x8Threads = [0.13553, 1.26, 20.11, 201.65]
-x16Threads = [0.13909, 1.21, 12.92, 207.41]
+x3Threads = [0.160, 1.35, 14.09, 253.62]
+x4threads = [0.12685, 1.07, 10.32, 184.91]
 
 y512 = [
-    ySequencialTime[0] / x2Threads[0],
-    ySequencialTime[0] / x4threads[0],
-    ySequencialTime[0] / x8Threads[0],
-    ySequencialTime[0] / x16Threads[0]
+    sequencial[0] / x1Thread[0],
+    sequencial[0] / x2Threads[0],
+    sequencial[0] / x3Threads[0],
+    sequencial[0] / x4threads[0],
 ]
 
 y1024 = [
-    ySequencialTime[1] / x2Threads[1],
-    ySequencialTime[1] / x4threads[1],
-    ySequencialTime[1] / x8Threads[1],
-    ySequencialTime[1] / x16Threads[1]
+    sequencial[1] / x1Thread[1],
+    sequencial[1] / x2Threads[1],
+    sequencial[1] / x3Threads[1],
+    sequencial[1] / x4threads[1],
 ]
 
-y2056 = [
-    ySequencialTime[2] / x2Threads[2],
-    ySequencialTime[2] / x4threads[2],
-    ySequencialTime[2] / x8Threads[2],
-    ySequencialTime[2] / x16Threads[2]
+y2048 = [
+    sequencial[2] / x1Thread[2],
+    sequencial[2] / x2Threads[2],
+    sequencial[2] / x3Threads[2],
+    sequencial[2] / x4threads[2],
 ]
 
 y4096 = [
-    ySequencialTime[3] / x2Threads[3],
-    ySequencialTime[3] / x4threads[3],
-    ySequencialTime[3] / x8Threads[3],
-    ySequencialTime[3] / x16Threads[3]
+    sequencial[3] / x1Thread[3],
+    sequencial[3] / x2Threads[3],
+    sequencial[3] / x3Threads[3],
+    sequencial[3] / x4threads[3],
 ]
 
-plt.plot(x, y512,
-         label="512", marker='o', linestyle='dashed')
-plt.plot(x, y1024,
-         label="1024", marker='o', linestyle='dashed')
-plt.plot(x, y2056,
-         label="2048", marker='o', linestyle='dashed')
-# plt.plot(x, y4096,
-#          label="4096", marker='o', linestyle='dashed')
+points = list(range(1, 5))
+plt.plot(points, points, label="ideal")
+plt.plot(points, y512, label="512")
+plt.plot(points, y1024, label="1024")
+plt.plot(points, y2048, label="2048")
+plt.plot(points, y4096, label="4096")
 
 plt.xlabel('Quantidade de Threads')
 plt.ylabel('Speedup')
